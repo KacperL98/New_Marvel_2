f60865a2db3928a78c7c17554110ea59f5a83486
KacperL98 <kacper.litwinow@gmail.com>
2021-02-11 23:25:27 +0100

    delete "?"

--- app/src/main/java/com/example/marvelapp/fragments/DetailsComicsFragment.kt
+++ app/src/main/java/com/example/marvelapp/fragments/DetailsComicsFragment.kt
@@ -3,29 +3,41 @@ package com.example.marvelapp.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.marvelapp.model.Result
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.DetailComicsFragmentBinding
+import com.example.marvelapp.databinding.FragmentListComicsBinding
 import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.detail_comics_fragment.*
 import timber.log.Timber
 
 @AndroidEntryPoint
-class DetailsComicsFragment : Fragment(R.layout.detail_comics_fragment) {
+class DetailsComicsFragment : Fragment() {
 
-    private var _binding: DetailComicsFragmentBinding? = null
-    private val binding get() = _binding
+    private lateinit var binding: DetailComicsFragmentBinding
     private var result: Result? = null
     private var creators: String = ""
     private var description: String = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DetailComicsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         result = arguments?.getParcelable("person_data") as Result?
-        _binding = DetailComicsFragmentBinding.bind(view)
+        binding = DetailComicsFragmentBinding.bind(view)
 
         comicBook.text = result?.title
 
@@ -37,14 +49,14 @@ class DetailsComicsFragment : Fragment(R.layout.detail_comics_fragment) {
         result?.creators?.items?.forEach {
             creators += "${it.name}\n"
         }
-        binding?.comicsBookAuthors?.text = creators
+        binding.comicsBookAuthors.text = creators
 
         result?.textObjects?.forEach {
             description += "${it.text}\n"
         }
-        binding?.descriptionTxt?.text = description
+        binding.descriptionTxt.text = description
 
-        binding?.btnLink?.setOnClickListener {
+        binding.btnLink.setOnClickListener {
             val website = result?.urls?.firstOrNull()
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(website?.url)
